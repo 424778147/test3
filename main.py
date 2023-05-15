6321def4f9a570f20763dc6978dc4db75fc77703
dima <dima.sonin.96@mail.ru>
2023-05-15 19:30:30 +0300

    better

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pytest
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        c = 1
+        c += 1
+        print(c)
         return str(x) == str(x)[::1]
 
 @pytest.mark.parametrize("input_value, expected", [(123, False),
                                                    (111, True),
-                                                   (222,True)])
+                                                   (222, True)])
 def test_palindrome(input_value, expected):
     sol = Solution()
     assert sol.isPalindrome(input_value) is expected
