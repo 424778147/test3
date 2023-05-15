3f9e85ead05fa486fdc2fb547f11ea6740cd34a2
dima <dima.sonin.96@mail.ru>
2023-05-15 18:58:52 +0300

    Lesson #1

---
+++ main.py
@@ -0,0 +1,13 @@
+import pytest
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::1]
+
+@pytest.mark.parametrize("input_value, expected", [(123, False),
+                                                   (111, True),
+                                                   (222,True)])
+def test_palindrome(input_value, expected):
+    sol = Solution()
+    assert sol.isPalindrome(input_value) is expected
+
